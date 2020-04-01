625ad7a1b9f2c589f5eaec0579034c03b965aa22
Alexander Ignatov <ani1802@abv.bg>
2020-04-01 21:30:44 +0300

    Refactor: use OOP

--- landbot.py
+++ landbot.py
@@ -7,25 +7,25 @@
 
 import discord
 
-client = discord.Client()
+
+class LandBot(discord.Client):
+    """Bot implementation."""
+
+    async def on_ready(self):
+        """Ouput useful debugging information."""
+        print(f"Bot logged in as {self.user}.")
+
+    async def on_message(self, message):
+        """Listen for messages starting with '!' and execute their commands."""
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("!test"):
+            await message.channel.send("Test? Why not.\nI am the best bot.")
 
 
-@client.event
-async def on_ready():
-    """Ouput useful debugging information."""
-    print(f"Bot logged in as {client.user}.")
-
-
-@client.event
-async def on_message(message):
-    """Listen for messages starting with '!' and execute their commands."""
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("!test"):
-        await message.channel.send("Test? Why not.\nI am the best bot.")
-
-
-with open("token", "r") as tokenfile:
-    token = tokenfile.read()[:-1]
-    client.run(token)
+if __name__ == "__main__":
+    client = LandBot()
+    with open("token", "r") as tokenfile:
+        token = tokenfile.read()[:-1]
+        client.run(token)
